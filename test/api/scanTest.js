e236ea064c1bcbf099e98af1ef54644dfc2041e3
Ken A. Redergård <ken.redergard@nordicsemi.no>
2015-10-26 15:11:06 +0100

    Fixing issue with scanTest after adapterFactory.getAdapters changed.

--- test/api/scanTest.js
+++ test/api/scanTest.js
@@ -1,31 +1,23 @@
 'use strict';
 
-var sinon = require('sinon');
-var assert = require('assert');
-var _ = require('underscore');
+const sinon = require('sinon');
+const assert = require('assert');
+const _ = require('underscore');
+
+const commonStubs = require('./commonStubs');
 
 var Adapter = require('../../api/adapter');
 
 describe('Adapter - Scanning', () => {
     let adapter;
-    let bleDriver;
     let bleDriverEventCallback;
 
     beforeEach(() => {
-        bleDriver =
-        {
-            start_scan: sinon.stub(),
-            stop_scan: sinon.stub(),
-            get_version: sinon.stub(),
-            open: (options, err) => {},
-            BLE_GAP_EVT_ADV_REPORT: 27
-        };
-        let open = sinon.stub(bleDriver, 'open', (port, options, callback) => {
-            bleDriverEventCallback = options.eventCallback;
-            callback();
+        this.bleDriver = commonStubs.createBleDriver(eventCallback => {
+            bleDriverEventCallback = eventCallback;
         });
 
-        adapter = new Adapter(bleDriver, 'COM1', 'COM1');
+        adapter = new Adapter(this.bleDriver, 'COM1', 'COM1');
 
         let options = {};
         adapter.open(options, err => {
@@ -34,9 +26,6 @@ describe('Adapter - Scanning', () => {
     });
 
     it('start and stop should provide callbacks with possible error and emit adapterStateChanged', (done) => {
-        bleDriver.start_scan.yieldsAsync(undefined);
-        bleDriver.stop_scan.yieldsAsync(undefined);
-
         let adapterStateChangedSpy = sinon.spy();
         adapter.on('adapterStateChanged', adapterStateChangedSpy);
 
@@ -61,8 +50,6 @@ describe('Adapter - Scanning', () => {
     });
 
     it('After starting scanning we should receive deviceDiscovered and deviceChanged events', (done) => {
-        bleDriver.start_scan.yieldsAsync(undefined);
-
         let deviceDiscoveredSpy = sinon.spy();
         adapter.on('deviceDiscovered', deviceDiscoveredSpy);
 
@@ -73,20 +60,24 @@ describe('Adapter - Scanning', () => {
             sinon.assert.notCalled(deviceDiscoveredSpy);
 
             const advReportUuids = ['0000180D-0000-1000-8000-00805F9B34FB'];
-            let advReportEvent = {id: bleDriver.BLE_GAP_EVT_ADV_REPORT,
-                                  peer_addr: {address: 'CD:96:E6:E2:3A:EA'},
-                                  data: {BLE_GAP_AD_TYPE_16BIT_SERVICE_UUID_COMPLETE: advReportUuids},
-                                  scan_rsp: false};
+            let advReportEvent = {
+                id: this.bleDriver.BLE_GAP_EVT_ADV_REPORT,
+                peer_addr: {address: 'CD:96:E6:E2:3A:EA'},
+                data: {BLE_GAP_AD_TYPE_16BIT_SERVICE_UUID_COMPLETE: advReportUuids},
+                scan_rsp: false,
+            };
             bleDriverEventCallback([advReportEvent]);
 
             sinon.assert.calledOnce(deviceDiscoveredSpy);
             assert.ok(_.isEqual(deviceDiscoveredSpy.lastCall.args[0].uuids, advReportUuids));
 
             const scanResponseUuids = ['0000180F-0000-1000-8000-00805F9B34FB'];
-            advReportEvent = {id: bleDriver.BLE_GAP_EVT_ADV_REPORT,
-                                  peer_addr: {address: 'CD:96:E6:E2:3A:EA'},
-                                  data: {BLE_GAP_AD_TYPE_16BIT_SERVICE_UUID_COMPLETE: scanResponseUuids},
-                                  scan_rsp: true};
+            advReportEvent = {
+                id: this.bleDriver.BLE_GAP_EVT_ADV_REPORT,
+                peer_addr: {address: 'CD:96:E6:E2:3A:EA'},
+                data: {BLE_GAP_AD_TYPE_16BIT_SERVICE_UUID_COMPLETE: scanResponseUuids},
+                scan_rsp: true,
+            };
             bleDriverEventCallback([advReportEvent]);
 
             sinon.assert.calledTwice(deviceDiscoveredSpy);
