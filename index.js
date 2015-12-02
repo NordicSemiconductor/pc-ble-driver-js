aedb078c38e0106f4bd12cc8d9f8fa1a1d95b699
Per Kristian Schanke <per.kristian.schanke@nordicsemi.no>
2015-12-02 16:12:54 +0100

    Case issues on Linux

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var AdapterState = require('./api/adapterState');
 var Characteristic = require('./api/characteristic');
 var Descriptor = require('./api/descriptor');
 var Device = require('./api/device');
-var Service = require('./api/Service');
-var ServiceFactory = require('./api/ServiceFactory');
+var Service = require('./api/service');
+var ServiceFactory = require('./api/serviceFactory');
 
 module.exports.driver = require('bindings')('ble_driver_js');
 module.exports.api = {
