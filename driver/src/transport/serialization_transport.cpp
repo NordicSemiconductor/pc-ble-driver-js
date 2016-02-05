fe0cd550dcd00f0efbd4e292fc22566a4b62d140
Simen Natvig <simen.natvig@nordicsemi.no>
2016-02-05 14:55:04 +0100

    Rename copy-pasted guard and mutex.

--- driver/src/transport/serialization_transport.cpp
+++ driver/src/transport/serialization_transport.cpp
@@ -92,7 +92,7 @@ uint32_t SerializationTransport::close()
 uint32_t SerializationTransport::send(uint8_t *cmdBuffer, uint32_t cmdLength, uint8_t *rspBuffer, uint32_t *rspLength)
 {
     // Mutex to avoid multiple threads sending commands at the same time.
-    std::unique_lock<std::mutex> responseGuard(responseMutex);
+    std::unique_lock<std::mutex> sendGuard(sendMutex);
     rspReceived = false;
     responseBuffer = rspBuffer;
     responseLength = rspLength;
