b2ed715c0013ed79f13e80bbc62375eb4d8c2b5e
Bjørn Inge Hanssen <bjorn.inge.hanssen@nordicsemi.no>
2016-02-17 14:42:30 +0100

    Add copyright header to source files

--- uuid_definitions.js
+++ uuid_definitions.js
@@ -1,4 +1,16 @@
-{
+/* Copyright (c) 2016 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+ {
     // Start Predefined names
     //
     {'uuid': 0x1800, 'name': 'GenericAccessProfile'},
