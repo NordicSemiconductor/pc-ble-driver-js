b07ee055b85f1809623d40e5a640525553fbc62c
kenr <ken.redergard@nordicsemi.no>
2016-03-16 12:56:25 +0100

    Fix issue with wrong keyset struct beeing sent to ble_gap_sec_keyset_t_dec.

--- driver/src/sdk/codec/ble_gap_evt_auth_status.c
+++ driver/src/sdk/codec/ble_gap_evt_auth_status.c
@@ -55,7 +55,7 @@ uint32_t ble_gap_evt_auth_status_dec(
     err_code = app_ble_gap_sec_context_find(p_event->evt.gap_evt.conn_handle, &keyset);
     SER_ASSERT(err_code == NRF_SUCCESS, err_code);
 
-    err_code = ble_gap_sec_keyset_t_dec(p_buf, packet_len, &index, keyset);
+    err_code = ble_gap_sec_keyset_t_dec(p_buf, packet_len, &index, static_cast<void*>(&keyset->keyset));
     SER_ASSERT(err_code == NRF_SUCCESS, err_code);
 
     err_code = app_ble_gap_sec_context_destroy(p_event->evt.gap_evt.conn_handle);
