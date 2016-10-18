05786fd89b80f6b15be87b40a79d6dd85227ba02
Terje Schjelderup <terje.schjelderup@nordicsemi.no>
2016-10-18 16:23:52 +0200

    WIP dfu tests

--- api/__tests__/dfu-test.js
+++ api/__tests__/dfu-test.js
@@ -9,7 +9,19 @@ describe('performDFU', () => {
 
     describe('when started without zip file path', () => {
         it('throws missing zip file path error', () => {
-            expect(() => dfu.performDFU()).toThrowError();
+            expect(() => dfu.performDFU(undefined, "adapter", "instance")).toThrow();
+        });
+    });
+
+    describe('when started without adapter', () => {
+        it('throws missing adapter error', () => {
+            expect(() => dfu.performDFU("path", undefined, "instance")).toThrow();
+        });
+    });
+
+    describe('when started without instance ID', () => {
+        it('throws missing instance ID error', () => {
+            expect(() => dfu.performDFU("path", "adapter", undefined)).toThrow();
         });
     });
 
