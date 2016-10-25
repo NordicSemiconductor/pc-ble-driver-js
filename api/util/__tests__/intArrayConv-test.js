6a7ce2e293f2cfd0209d7240efadc6288bfe3e3f
Terje Schjelderup <terje.schjelderup@nordicsemi.no>
2016-10-25 13:54:48 +0200

    Add input checks for array/int conversions.

--- api/util/__tests__/intArrayConv-test.js
+++ api/util/__tests__/intArrayConv-test.js
@@ -18,11 +18,11 @@ describe('Integer/array conversions', () => {
 
         it('should pad the resulting array with 0x00', () => {
             expect(intToArray(0x01, 4)).toEqual([0x01, 0x00, 0x00, 0x00]);
-        })
+        });
 
         it ('should convert 0x123456 to [0x56, 0x34, 0x12]', () => {
             expect(intToArray(0x123456, 3)).toEqual([0x56, 0x34, 0x12]);
-        })
+        });
 
         // TODO: More test cases for correct behaviour, especially edge cases.
     });
@@ -30,24 +30,28 @@ describe('Integer/array conversions', () => {
     describe('when converting arrays to integers', () => {
 
         it('should throw if the argument is not an array', () => {
-            expect(arrayToInt({})).toThrow();
-            expect(arrayToInt(42)).toThrow();
-            expect(arrayToInt("string")).toThrow();
+            expect(() => arrayToInt({})).toThrow();
+            expect(() => arrayToInt(42)).toThrow();
+            expect(() => arrayToInt("string")).toThrow();
         });
 
         it('should throw if the array contains non-integers', () => {
-            expect(arrayToInt([82, 129, "baz"])).toThrow();
-            expect(arrayToInt([{}, 42])).toThrow();
+            expect(() => arrayToInt([82, 129, "baz"])).toThrow();
+            expect(() => arrayToInt([{}, 42])).toThrow();
         });
 
         it('should throw if the array values are not in (0x00, 0xFF)', () => {
-            expect(arrayToInt([17, -3])).toThrow();
-            expect(arrayToInt([42, 0x100])).toThrow();
+            expect(() => arrayToInt([17, -3])).toThrow();
+            expect(() => arrayToInt([42, 0x100])).toThrow();
         });
 
         it('should convert [0x00] to 0', () => {
             expect(arrayToInt([0x00])).toEqual(0);
         });
+
+        it ('should convert [0x34, 0x12] to 0x1234', () => {
+            expect(arrayToInt([0x34, 0x12])).toEqual(0x1234);
+        })
 
         // TODO: More test cases for correct behaviour, especially edge cases.
     });
