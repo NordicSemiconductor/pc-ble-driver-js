80453365883956d7141caa82cc8d0dabe54791be
Magne Rodem <magne@rodem.no>
2016-10-25 09:06:05 +0200

    Add more tests for object writer

--- api/dfu/__tests__/dfuObjectWriter-test.js
+++ api/dfu/__tests__/dfuObjectWriter-test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { ControlPointOpcode } = require('../dfuConstants');
 const DfuObjectWriter = require('../dfuObjectWriter');
 
 describe('writeObject', () => {
@@ -18,40 +19,37 @@ describe('writeObject', () => {
         objectWriter._notificationStore = notificationStore;
     });
 
-    describe('when write packets called', () => {
 
-        describe('when write packets complete', () => {
+    describe('when writing packets has succeeded', () => {
 
-            const accumulatedCrc = 123;
+        const accumulatedCrc = 123;
 
-            beforeEach(() => {
-                // Inject our own packet writer that returns successfully,
-                // and has an accumulated CRC.
-                objectWriter._createPacketWriter = () => {
-                    return {
-                        writePacket: () => Promise.resolve(),
-                        getAccumulatedCrc: () => accumulatedCrc
-                    };
+        beforeEach(() => {
+            // Inject our own packet writer that returns successfully,
+            // and has an accumulated CRC.
+            objectWriter._createPacketWriter = () => {
+                return {
+                    writePacket: () => Promise.resolve(),
+                    getAccumulatedCrc: () => accumulatedCrc
                 };
-            });
+            };
+        });
 
-            it('should return accumulated CRC', () => {
-                return objectWriter.writeObject([1]).then(crc => {
-                    expect(crc).toEqual(accumulatedCrc);
-                });
+        it('should return accumulated CRC', () => {
+            return objectWriter.writeObject([1]).then(crc => {
+                expect(crc).toEqual(accumulatedCrc);
             });
+        });
 
-            it('should stop listening to notifications', () => {
-                return objectWriter.writeObject([1]).then(() => {
-                    expect(notificationStore.stopListening).toHaveBeenCalled();
-                });
+        it('should stop listening to notifications', () => {
+            return objectWriter.writeObject([1]).then(() => {
+                expect(notificationStore.stopListening).toHaveBeenCalled();
             });
-
         });
 
     });
 
-    describe('when write packets failed', () => {
+    describe('when writing packets have failed', () => {
 
         it('should re-throw error', () => {
             objectWriter._writePackets = () => Promise.reject('Some error');
@@ -65,6 +63,41 @@ describe('writeObject', () => {
             return objectWriter.writeObject([1]).catch(() => {
                 expect(notificationStore.stopListening).toHaveBeenCalled();
             });
+        });
+    });
+
+    describe('when packet writer returns CRC', () => {
+
+        const crc = 123;
+
+        beforeEach(() => {
+            // Inject our own packet writer that returns CRC.
+            objectWriter._createPacketWriter = () => {
+                return {
+                    writePacket: () => Promise.resolve(crc),
+                    getAccumulatedCrc: jest.fn()
+                };
+            };
+        });
+
+        describe('when match with CRC from last notification', () => {
+
+            it('should complete without error', () => {
+                notificationStore.readLatest = () => Promise.resolve({ crc: 123 });
+                return objectWriter.writeObject([1]).then(() => {});
+            });
+
+        });
+
+        describe('when mismatch with CRC from last notification', () => {
+
+            it('should return error', () => {
+                notificationStore.readLatest = () => Promise.resolve({ crc: 456 });
+                return objectWriter.writeObject([1]).catch(error => {
+                    expect(error.message).toContain('Error when validating CRC');
+                });
+            });
+
         });
     });
 
