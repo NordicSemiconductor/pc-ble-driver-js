2ab209cfcfd75cb04627c24fa3bc65c860bdd09f
Michael <michael.dietz@waya.ai>
2017-02-28 15:31:22 -0600

    add detailed desc of available properties and options when creating charactersitcs/descriptors

--- api/serviceFactory.js
+++ api/serviceFactory.js
@@ -45,7 +45,7 @@ const Descriptor = require('./descriptor');
 
 class ServiceFactory {
     /**
-     * Factory method to create a service in the Bluetooth 'local.server' `Device's` GATT attribute table.
+     * Factory method to create a service in the Bluetooth `Device's` 'local.server' GATT attribute table.
      *
      * @param {string} uuid A 128-bit or 16-bit unique identifier for this service.
      * @param {string} serviceType The server service type. 'primary' (default) or `secondary`.
@@ -58,26 +58,44 @@ class ServiceFactory {
     /**
      * Factory method to create a characteristic in the Bluetooth `Device's` GATT attribute table.
      *
-     * @param {string} service The `Service` instance this characteristic is to be added to.
+     * @param {Service} service The `Service` instance this characteristic is to be added to.
      * @param {string} uuid A 128-bit or 16-bit unique identifier for this characteristic.
      * @param {array} value The initial value of this characteristic.
+     *
+     * Available characteristic properties:
+     * - {boolean} broadcast: Broadcasting of the value permitted.
+     * - {boolean} read: Reading the value permitted.
+     * - {boolean} write: Writing the value with Write Request permitted.
+     * - {boolean} writeWoResp: Writing the value with Write Command permitted.
+     * - {boolean} reliableWrite: Writing the value with Queued Write operations permitted.
+     * - {boolean} notify: Notications of the value permitted.
+     * - {boolean} indicate: Indications of the value permitted.
+     * - {boolean} authSignedWr: Writing the value with Signed Write Command permitted.
+     * - {boolean} wrAux: Writing the Characteristic User Description descriptor permitted.
+     *
      * @param {Object} properties This GATT characteristic's metadata.
+     *
+     * Available characteristic options:
+     * - {string} readPerm: Read permissions.
+     * - {string} writePerm: Write permissions.
+     * - {boolean} variableLength: Variable length attribute.
+     * - {number} maxLength: Maximum attribute value length in bytes, see ref BLE_GATTS_ATTR_LENS_MAX for maximum values.
+     * - {boolean} readAuth: Read authorization and value will be requested from the application on every read operation.
+     * - {boolean} writeAuth: Write authorization will be requested from the application on every Write Request operation (but not Write Command).
+     * Note: vloc = this._bleDriver.BLE_GATTS_VLOC_STACK; // Attribute Value is located in stack memory, no user memory is required.
      * @param {Object} options This GATT characteristic's attribute's metadata.
      * @returns {Characteristic} A newly created `Characteristic` instance.
      */
     createCharacteristic(service, uuid, value, properties, options) {
-        if (!service) throw new Error('Service to add characteristics to must be provided.');
+        if (!service) {
+            throw new Error('Service to add characteristics to must be provided.');
+        }
 
         if (service._factory_characteristics === undefined) {
             service._factory_characteristics = [];
         }
 
-        const characteristic = new Characteristic(
-            service.instanceId,
-            uuid,
-            value,
-            properties,
-            options);
+        const characteristic = new Characteristic(service.instanceId, uuid, value, properties, options);
 
         service._factory_characteristics.push(characteristic);
         return characteristic;
@@ -86,14 +104,25 @@ class ServiceFactory {
     /**
      * Factory method to create a descriptor in the Bluetooth `Device's` GATT attribute table.
      *
-     * @param {string} characteristic The `Characteristic` instance this descriptor is to be added to.
+     * @param {Characteristic} characteristic The `Characteristic` instance this descriptor is to be added to.
      * @param {string} uuid A 128-bit or 16-bit unique identifier for this descriptor.
      * @param {array} value The initial value of this descriptor.
+     *
+     * Available descriptor options:
+     * - {string} readPerm: Read permissions.
+     * - {string} writePerm: Write permissions.
+     * - {boolean} variableLength: Variable length attribute.
+     * - {number} maxLength: Maximum attribute value length in bytes, see ref BLE_GATTS_ATTR_LENS_MAX for maximum values.
+     * - {boolean} readAuth: Read authorization and value will be requested from the application on every read operation.
+     * - {boolean} writeAuth: Write authorization will be requested from the application on every Write Request operation (but not Write Command).
+     * Note: vloc = this._bleDriver.BLE_GATTS_VLOC_STACK; // Attribute Value is located in stack memory, no user memory is required.
      * @param {Object} options This GATT descriptor's attribute's metadata.
      * @returns {Descriptor} A newly created `Descriptor` instance.
      */
     createDescriptor(characteristic, uuid, value, options) {
-        if (!characteristic) throw new Error('Characteristic to add descriptor to must be provided.');
+        if (!characteristic) {
+            throw new Error('Characteristic to add descriptor to must be provided.');
+        }
 
         if (characteristic._factory_descriptors === undefined) {
             characteristic._factory_descriptors = [];
