d1f45ffb2cd06e03145d462aceb1d68d1e04de42
Ken A. Redergård <ken.redergard@nordicsemi.no>
2018-05-02 15:53:11 +0200

    bump nrf-device-setup version move adapter pool functionality into setup (adapterPool.js will be removed and examples directory will be updated) add releaseAdapter functionality add support for determining SoftDevice parameters given a serial number (Segger, Nordic DFU based device)

--- test/setup.js
+++ test/setup.js
@@ -37,7 +37,6 @@
 'use strict';
 
 const api = require('../index');
-const { adapterPool } = require('../examples/adapterPool');
 
 const adapterFactory = api.AdapterFactory.getInstance();
 const serviceFactory = new api.ServiceFactory();
@@ -47,15 +46,209 @@ const testTimeout = 2000;
 const log = require('debug')('setup:log');
 const error = require('debug')('setup:error');
 
-// Function shall return a working opened adapter ready for use
+const DeviceLister = require('nrf-device-lister');
+const { setupDevice } = require('nrf-device-setup');
+const { FirmwareRegistry } = require('../index');
+
+const traits = {
+    usb: false,
+    nordicUsb: true,
+    seggerUsb: false,
+    serialport: true,
+    nordicDfu: true,
+    jlink: true,
+};
+
+const deviceLister = new DeviceLister(traits);
+const grabbedAdapters = new Map();
+
+// Make sure the device lister does not crash if a listener for an error event is not registered.
+// The device lister emits errors that will not affect us.
+deviceLister.on('error', () => {
+});
+
+function getAdapters() {
+    return deviceLister.reenumerate()
+        .then(adapters => {
+            const result = new Map();
+
+            // Only pick adapters we are able to program
+            adapters.forEach((adapter, serialNumber) => {
+                const add = adapter.traits
+                    && (adapter.traits.includes('jlink') || adapter.traits.includes('nordicDfu'));
+
+                if (add) {
+                    result.set(serialNumber, adapter);
+                }
+            });
+
+            return result;
+        });
+}
+
+/**
+ * @typedef {Object} SoftDeviceParams
+ * @property {string} sdVersion SoftDevice API version
+ * @property {number} baudRate Baudrate to use for connectivity firmware UART communication
+ */
+
+/**
+ * Determine SoftDevice parameters to use
+ *
+ * This function/functionality should be moved to nrf-device-setup and reused from that location
+ * so that it lives in one central places
+ *
+ * @param {string} serialNumber to use for determining the SoftDevice parameters to use
+ * @returns {SoftDeviceParams} SoftDevice parameters to use for communicating with the connectivity firmware
+ *
+ */
+function determineSoftDeviceParameters(serialNumber) {
+    const seggerSerialNumber = /^.*68([0-3]{1})[0-9]{6}$/;
+    const gravitonDongleSerialNumber = /^[a-fA-F0-9]{12}$/;
+    const params = {};
+
+    params.baudRate = process.platform === 'darwin' ? 115200 : 1000000;
+
+    if (seggerSerialNumber.test(serialNumber)) {
+        const developmentKit = parseInt(seggerSerialNumber.exec(serialNumber)[1], 10);
+
+        switch (developmentKit) {
+            case 0:
+            case 1:
+                params.sdVersion = 'v2';
+                break;
+            case 2:
+                params.sdVersion = 'v3';
+                break;
+            case 3:
+                params.sdVersion = 'v3';
+                break;
+            default:
+                throw new Error(`Unsupported nRF5 development kit. [${serialNumber}]`);
+        }
+    } else if (gravitonDongleSerialNumber.test(serialNumber)) {
+        params.sdVersion = 'v3';
+    } else {
+        throw new Error(`Not able to determine version of pc-ble-driver to use for device with serial number ${serialNumber}.`);
+    }
+
+    return params;
+}
+
+async function _grabAdapter(serialNumber) {
+    const foundAdapters = await getAdapters();
+
+    const adapterToUse = () => {
+        const serialNumbers = [...foundAdapters.keys()];
+
+        if (serialNumber != null) {
+            // Interim solution while waiting for:
+            //   https://github.com/NordicSemiconductor/nrf-device-lister-js/pull/28
+            const p = serialNumbers.includes(serialNumber);
+            const q = serialNumbers.includes(Number.parseInt(serialNumber, 10));
+
+            if (!(p || q)) {
+                throw new Error(`Adapter with serial number ${serialNumber} does not exist.`);
+            }
+
+            const sn = p ? serialNumber : Number.parseInt(serialNumber, 10);
+
+            grabbedAdapters.set(sn, foundAdapters.get(serialNumber));
+            return foundAdapters.get(sn);
+        }
+
+        // Grab an adapter that has not been used before
+        let availableAdapter = null;
+
+        if (serialNumbers.some(sn => {
+            if (![...grabbedAdapters.keys()].includes(sn)) {
+                availableAdapter = foundAdapters.get(sn);
+                return true;
+            }
+
+            return false;
+        })) {
+            return availableAdapter;
+        }
+
+        return null;
+    };
+
+    const selectedAdapter = adapterToUse();
+
+    if (selectedAdapter == null) {
+        throw new Error('No adapter up for grabs.');
+    }
+
+    // TODO: when DFU works, uncomment setupDevice
+    // await setupDevice(selectedAdapter, FirmwareRegistry.getDeviceSetup());
+    const softDeviceParams = determineSoftDeviceParameters(selectedAdapter.serialNumber);
+
+    return {
+        serialNumber: selectedAdapter.serialNumber,
+        port: selectedAdapter.serialport.comName,
+        apiVersion: softDeviceParams.sdVersion,
+        baudRate: softDeviceParams.baudRate,
+    };
+}
+
+/**
+ * Release a previously grabbed adapter.
+ *
+ * The adapter is removed from the list of grabbed adapters and the UART connection to the adapter is closed.
+ *
+ * @param {string} serialNumber Serial number of device to release, if not provided and only one adapter has been grabbed, release the one. If not reject.
+ * @returns {Promise<serialNumber>} Resolved promise with released adapter if OK, rejected if not
+ */
+async function releaseAdapter(serialNumber) {
+    if (!grabbedAdapters || grabbedAdapters.length === 0) {
+        throw new Error('No adapters in adapter pool to release.');
+    }
+
+    if (!serialNumber && grabbedAdapters.length > 1) {
+        throw new Error('More than one adapter is grabbed. Not able to determine which one to release.');
+    }
+
+    let adapterToReleaseSn = serialNumber;
+    let adapterToRelease;
+
+    if (!serialNumber) {
+        adapterToReleaseSn = [...grabbedAdapters.keys()][0];
+        adapterToRelease = grabbedAdapters.get(adapterToReleaseSn);
+    } else {
+        adapterToRelease = grabbedAdapters.get(serialNumber);
+
+        if (!adapterToRelease) {
+            throw new Error(`Adapter serial number ${serialNumber} is not in the list of grabbed adapters.`);
+        }
+    }
+
+    grabbedAdapters.delete(adapterToReleaseSn);
+
+    return new Promise((resolve, reject) => {
+        adapterToRelease.close(closeError => {
+            if (closeError) {
+                reject(error);
+                return;
+            }
+
+            resolve(adapterToReleaseSn);
+        });
+    });
+}
+
+/**
+ * Grab an available adapter.
+ *
+ * @param {(string|undefined)} requestedSerialNumber Specific adapter to grab, if undefined, the function picks one that is not registered as grabbed from before
+ * @returns {Promise<Adapter>} An opened adapter ready to use
+ */
 async function grabAdapter(requestedSerialNumber) {
-    const { apiVersion, port, serialNumber } = await adapterPool(requestedSerialNumber);
+    const { port, serialNumber, apiVersion, baudRate } = await _grabAdapter(requestedSerialNumber);
     const adapter = adapterFactory.createAdapter(apiVersion, port, serialNumber);
 
     adapter.on('error', err => error(`Error adapter ${err}`));
-
-    // TODO: automatically determine baud rate
-    const baudRate = 1000000;
+    grabbedAdapters.set(serialNumber, adapter);
 
     return new Promise((resolve, reject) => {
         adapter.open({ baudRate }, err => {
@@ -69,6 +262,13 @@ async function grabAdapter(requestedSerialNumber) {
     });
 }
 
+/**
+ * Await outcome of an array of promises
+ *
+ * @param {Array<Promise>} futureOutcomes An array of Promises to resolve
+ * @param {number} timeout Milliseconds to wait before outcome is regarded as timed out
+ * @returns {Promise<any>} An array of the promises.
+ */
 async function outcome(futureOutcomes, timeout) {
     let timeoutId = null;
 
@@ -95,6 +295,15 @@ function addAdapterListener(adapter, prefix) {
     adapter.on('deviceDiscovered', device => { log(`${prefix} deviceDiscovered: ${JSON.stringify(device)}`); });
 }
 
+/**
+ * Setup adapter with logging, name to advertise with and device address to
+ * @param {Adapter} adapter Adapter to setup
+ * @param {string} prefix Log prefix to use for this adapter
+ * @param {string} name Name to set in SoftDevice for this device
+ * @param {string} address Bluetooth Low Energy address to set for this device. Format is XX:XX:XX:XX:XX:XX
+ * @param {string} addressType Bluetooth Log Energy address type to set for this device.
+ * @returns {Promise<Adapter>} Adapter that has been setup. Same as the adapter provided as in argument.
+ */
 function setupAdapter(adapter, prefix, name, address, addressType) {
     return new Promise((resolve, reject) => {
         addAdapterListener(adapter, prefix);
@@ -127,8 +336,8 @@ function setupAdapter(adapter, prefix, name, address, addressType) {
 module.exports = {
     adapterFactory,
     serviceFactory,
-    adapterPool,
     grabAdapter,
+    releaseAdapter,
     setupAdapter,
     outcome,
 };
